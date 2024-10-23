941249aad384b2cee9006726fafa4724f9b21a73
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-10-23 21:16:23 +0500

    Done

--- App.jsx
+++ App.jsx
@@ -1,19 +1,34 @@
-import { View, Text, TouchableOpacity, Button, Image, TextInput, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, Button, Image, TextInput, ScrollView, Alert } from 'react-native'
 import React, { useState } from 'react'
 
 const App = () => {
-  const [value, setValue] = useState('SMIT');
+  const [value, setValue] = useState([]);
   
+
+  const AddTodo = () => {
+    const oldTodos = [...value]
+    oldTodos.push(value)
+    setValue(oldTodos)
+    setValue('')
+  }
   return (
-    <View style={{flex: 1, width: '100%', height: '100%'}}>
-<View style={{flex: 1, backgroundColor: 'orange', justifyContent: 'center', alignItems: 'center'}}>
-  <Text style={{fontSize: 30, color: 'black'}}>Todo App</Text>
-</View>
-<View style={{flex: 5, backgroundColor: 'green', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 10}}>
-  <TextInput style={{borderWidth: 1, width: '80%', padding: 10, borderRadius: 10}} placeholder='Enter Todo'/>
-  <TouchableOpacity>
-    
+    <View style={{flex: 1, width: '100%'}}>
+<View style={{flex: 5, justifyContent: 'center', alignItems: 'center', gap: 20}}>
+<Text style={{fontSize: 30, color: 'black', marginBottom: 20}}>Todo App</Text>
+  <TextInput 
+  placeholderTextColor={'#000000'}
+  value={value}
+  onChangeText={(text)=> setValue(text)}
+  style={{borderWidth: 1, width: '80%', padding: 10, borderRadius: 10}} placeholder='Enter Todo'/>
+  <TouchableOpacity 
+  activeOpacity={0.5}
+  onPress={()=> AddTodo()}
+  style={{backgroundColor: 'red', padding: 10, borderRadius: 10, width: '80%'}}>
+    <Text style={{color: 'white', textAlign: 'center'}}>Submit</Text>
   </TouchableOpacity>
+</View>
+<View style={{flex: 1}}>
+  <Text style={{color: 'black', fontSize: 20, textAlign: 'center', flexDirection: 'row'}}>{value}</Text>
 </View>
 </View>
    
