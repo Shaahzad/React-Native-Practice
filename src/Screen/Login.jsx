53fd95e02d45a3d4d6f97aa1e8213dd3f5d3e90f
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-10-27 21:47:40 +0500

    Done

--- src/Screen/Login.jsx
+++ src/Screen/Login.jsx
@@ -1,11 +1,11 @@
-import { View, Text, Image, TextInput } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import IonIcon from "react-native-vector-icons/Ionicons"
 export default function Login() {
   return (
     <View style={{flex: 1}}>
       <View style={{flex: 1, backgroundColor: 'blue'}}>
-        <View style={{flexDirection: 'row', marginTop: "10%", alignItems: 'center', marginBottom: '45%'}}>
+        <View style={{flexDirection: 'row', alignItems: 'center', paddingVertical: 30}}>
         <View style={{paddingLeft: 10}}>
           <IonIcon name='chevron-back' size={25} color={'white'}/>
         </View>
@@ -15,18 +15,20 @@ export default function Login() {
           </Text>
         </View>
         </View>
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{flex: 1, alignItems: 'center'}}>
           <Image source={require('../assets/Banner.png')}/>
         </View>
       </View>
       <View style={{flex: 1}}>
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', gap: 10}}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', gap: 10, marginTop: 20}}>
         <TextInput placeholder='Email Address' style={{borderWidth: 1, width: '80%', borderRadius: 5, padding: 10}}/>
         <TextInput placeholder='Password' secureTextEntry={true} style={{borderWidth: 1, width: '80%', borderRadius: 5, padding: 10}}/>
-        <View style={{width: '80%', alignItems: 'flex-end'}}>
-          <Text style={{fontSize: 15, fontWeight: 'bold', color: 'black'}}>Forget Password ?</Text>
-          </View>
-      </View>
+        <Text style={{fontSize: 15, fontWeight: 'bold', color: 'black', width: '80%', textAlign: 'right', marginBottom: 10}}>Forget Password ?</Text>
+        <TouchableOpacity activeOpacity={0.5} style={{backgroundColor: 'blue', padding: 15, width: '80%', borderRadius: 5, flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold', paddingLeft: 10}}>Login</Text>
+            <IonIcon name='chevron-forward' size={25} color={'white'} style={{paddingLeft: 10}}/>
+          </TouchableOpacity> 
+      </View>  
       </View>
     </View>
   )
