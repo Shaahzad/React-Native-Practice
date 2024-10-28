3a7688d77d65688eaf93e242c8a700de9b81c301
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-10-28 23:32:46 +0500

    dONE

--- src/Screen/Dashboard.jsx
+++ src/Screen/Dashboard.jsx
@@ -18,11 +18,19 @@ export default function Dashboard() {
         </View>
       </LinearGradient>
     </View>
-    <View style={{flex: 6}}>
-        <View style={{flex: 1, padding: 20}}>
+    <View style={{flex: 1}}>
+    <View style={{flex: 1, padding: 20}}>
       <Text style={{fontSize: 18, fontWeight: 'bold'}}>Asslam u Alikum</Text>
       <Text style={{fontSize: 22, fontWeight: 'bold', color: '#1A2980'}}>Usama Hussain</Text>
-        </View>
+    </View>
+    </View>
+    <View style={{flex: 1}}>
+    <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#FFFFFF', '#F9F9F9', '#F2F2F2']} style={{flex: 1}}>
+     <View>
+        <Text>Programming Course</Text>
+        <Text>Php, Laravel, Mysql</Text>
+     </View>
+    </LinearGradient>
     </View>
     </View>
   )
